7533eb2e8121bec505c6983165a764db368f03b9
duytruong-23 <zduytruongz@gmail.com>
2023-07-18 16:30:32 +0700

    Thêm giới thiệu trang

--- client/src/components/nav/Navbar.js
+++ client/src/components/nav/Navbar.js
@@ -1,27 +1,27 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <nav className="hidden lg:flex lg:justify-around lg:items-center lg:my-4">
-            <Link to="/" className="home hover:opacity-50  hover-effect">
-                Trang chủ
-            </Link>
-            <Link to="/products" className="home hover:opacity-50  hover-effect">
-                Tất cả Sản phẩm
-            </Link>
-            <Link to="/keyboard" className="home hover:opacity-50  hover-effect">
-                Bàn phím cơ
-            </Link>
-            <Link to="/keycap" className="home hover:opacity-50  hover-effect">
-                Keycap
-            </Link>
-            <Link to="/kit" className="home hover:opacity-50  hover-effect">
-                Kit
-            </Link>
-            <Link to="/about" className="home hover:opacity-50  hover-effect">
-                Về keycap
-            </Link>
-        </nav>
-    );
+  return (
+    <nav className="hidden lg:flex lg:justify-around lg:items-center lg:my-4">
+      <Link to="/" className="home hover:opacity-50  hover-effect">
+        Trang chủ
+      </Link>
+      <Link to="/products" className="home hover:opacity-50  hover-effect">
+        Tất cả Sản phẩm
+      </Link>
+      <Link to="/keyboard" className="home hover:opacity-50  hover-effect">
+        Bàn phím cơ
+      </Link>
+      <Link to="/keycap" className="home hover:opacity-50  hover-effect">
+        Keycap
+      </Link>
+      <Link to="/kit" className="home hover:opacity-50  hover-effect">
+        Kit
+      </Link>
+      <Link to="/about" className="home hover:opacity-50  hover-effect">
+        Giới thiệu trang
+      </Link>
+    </nav>
+  );
 };
 export default Navbar;
--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -1,10 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 const AboutPage = () => {
-    useEffect(() => {
-        document.title = 'About';
-    }, []);
+  useEffect(() => {
+    document.title = "About";
+  }, []);
 
-    return <div>AboutPage</div>;
+  return (
+    <div>
+      Chúng tôi ra đời với sứ mệnh mang đến không gian làm việc độc đáo và sáng
+      tạo cho mọi người.
+      <br />
+      <b>CẢM HỨNG HƠN?</b>
+      <br />
+      Đó luôn là mục tiêc chúng tôi hướng đến.
+      <br />
+      Và cũng là tinh thần "More inspirational" chúng tôi khát khao truyền tải.
+      <br />
+      Với những sản phẩm được lựa chọn một cách tỉ mỉ và cẩn thận trong khâu
+      đánh giá chất lượng. Chắc chắn, điều này sẽ giúp các bạn có một trải
+      nghiệm hoàn hảo khi sử dụng sản phẩm của chúng tôi. Chúc các bạn luôn vui
+      vẻ, sáng tạo và tràn đầy năng lượng mỗi khi bắt đầu làm việc. Thanks!
+    </div>
+  );
 };
 export default AboutPage;
