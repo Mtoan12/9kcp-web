a4d4dae708e0a027d3114cb42e6a452a1cc9d747
duytruong-23 <zduytruongz@gmail.com>
2023-07-18 16:56:21 +0700

    chỉnh giới thiệu trang

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -6,20 +6,30 @@ const AboutPage = () => {
   }, []);
 
   return (
-    <div>
-      Chúng tôi ra đời với sứ mệnh mang đến không gian làm việc độc đáo và sáng
-      tạo cho mọi người.
-      <br />
-      <b>CẢM HỨNG HƠN?</b>
-      <br />
-      Đó luôn là mục tiêc chúng tôi hướng đến.
-      <br />
-      Và cũng là tinh thần "More inspirational" chúng tôi khát khao truyền tải.
-      <br />
-      Với những sản phẩm được lựa chọn một cách tỉ mỉ và cẩn thận trong khâu
-      đánh giá chất lượng. Chắc chắn, điều này sẽ giúp các bạn có một trải
-      nghiệm hoàn hảo khi sử dụng sản phẩm của chúng tôi. Chúc các bạn luôn vui
-      vẻ, sáng tạo và tràn đầy năng lượng mỗi khi bắt đầu làm việc. Thanks!
+    <div className="lg:container lg:px-5">
+      <div className="lg:grid lg:grid-cols-1">
+        <p>
+          Chúng tôi ra đời với sứ mệnh mang đến không gian làm việc độc đáo và
+          sáng tạo cho mọi người.
+        </p>
+        <br />
+        <b>CẢM HỨNG HƠN?</b>
+        <br />
+        <p>Đó luôn là mục tiêc chúng tôi hướng đến.</p>
+        <br />
+        <p>
+          Và cũng là tinh thần "More inspirational" chúng tôi khát khao truyền
+          tải.
+        </p>
+        <br />
+        <p>
+          Với những sản phẩm được lựa chọn một cách tỉ mỉ và cẩn thận trong khâu
+          đánh giá chất lượng. Chắc chắn, điều này sẽ giúp các bạn có một trải
+          nghiệm hoàn hảo khi sử dụng sản phẩm của chúng tôi. Chúc các bạn luôn
+          vui vẻ, sáng tạo và tràn đầy năng lượng mỗi khi bắt đầu làm việc.
+          Thanks!
+        </p>
+      </div>
     </div>
   );
 };
